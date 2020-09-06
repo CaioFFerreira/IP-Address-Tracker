7553729b245447e6f0ca84b1d4f08c6134a49b21
caio fabio <caio_fbo@hotmail.com>
2020-09-05 23:13:33 -0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,37 +1,64 @@
 
- var map, infoWindow;
-    function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 6
-        });
-        infoWindow = new google.maps.InfoWindow;
+const inputElement = document.querySelector('#search');
+const bntElement = document.querySelector('.btn');
+const mapa = document.querySelector('.mapa');
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+function BuscarMapa(){
+    var ipAddress = inputElement.value;
+    const api = 'https://geo.ipify.org/api/v1?apiKey=at_kkV0RSKzsozxyqc3oIFGd6H5GY5ex&ipAddress=' + ipAddress;
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+    $(document).ready(function(){
+     $.getJSON(api , function(data){
+  
+         var ip = '';
+         var location = '';
+         var isp = '';
+         var timezone ='';
+ 
+         var lat ='';
+         var lng ='';
+ 
+         ip +=  data.ip;
+         location += data.location.region + ', ' + data.location.city + '-' + data.location.country;
+         isp += data.isp;
+         timezone += data.location.timezone;
+ 
+         lat+= data.location.lat;
+         lng+= data.location.lng;
+         
+        $('#ip').html(ip);
+        $('#location').html(location);
+        $('#isp').html(isp);
+        $('#timezone').html(timezone);
+        
+        if (L.DomUtil.get(mapa) !== undefined) { 
+            L.DomUtil.get(mapa)._leaflet_id = null; 
+         }  
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+         
+        var mymap = L.map(mapa).setView([lat, lng], 10);
+         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+             maxZoom: 18,
+             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+             id: 'mapbox/streets-v11',
+             tileSize: 512,
+             zoomOffset: -1
+         }).addTo(mymap);
+ 
+         var icon = L.icon({
+             iconUrl: '../images/icon-location.svg',
+             iconAnchor:   [22, 54],
+             popupAnchor:  [-3, -76]
+         });
+ 
+         L.marker([lat, lng] , {icon: icon}).addTo(mymap);
+     });
+ });
+
+}
+
+bntElement.addEventListener('click',BuscarMapa );
+
+
